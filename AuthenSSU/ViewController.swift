7440638874a44d411e15412e46b57a759eebfc79
Student10 <student10@Student10.local>
2019-03-20 14:08:37 +0700

    Get Event From Click Success

--- AuthenSSU/ViewController.swift
+++ AuthenSSU/ViewController.swift
@@ -8,15 +8,26 @@
 
 
 
+
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    } //Mailn Method
+    
+    
+    @IBAction func registerButton(_ sender: UIButton) {
+        
+        print("You Click Register")
+        
     }
-
-
-}
+    
+    
+} //Main Class
 
