f8f772dd91f596c123d60c2dde1e9fb485ec2a8a
Student10 <student10@Student10.local>
2019-03-21 11:29:33 +0700

    Check Space Success

--- AuthenSSU/RegisterViewController.swift
+++ AuthenSSU/RegisterViewController.swift
@@ -44,8 +44,18 @@ class RegisterViewController: UIViewController {
         print("user =\(user)")
         print("password =\(password)")
         
+        if (name.count == 0) || (user.count == 0) || (password.count == 0) {
+            myAlert(titleString: "Have Space", messageSting: "Please Fill All Blank")
+        }else{
+            
+        }
         
         
+    } // upload Button
+    
+    func myAlert(titleString:String,messageSting:String) -> Void{
+        
+        print("title = \(titleString),message = \(messageSting)")
         
     }
     
