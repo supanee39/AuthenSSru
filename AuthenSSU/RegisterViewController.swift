c907e205066e409cdd1cf4670e4ac941791f2c87
Student10 <student10@Student10.local>
2019-03-21 15:14:57 +0700

    Upload Data Success

--- AuthenSSU/RegisterViewController.swift
+++ AuthenSSU/RegisterViewController.swift
@@ -64,9 +64,15 @@ class RegisterViewController: UIViewController {
         
         present(alert,animated: true,completion: nil)
         
+        }  //myAlert
+    
+    func uploadData(name: String,user: String,password: String) -> Void {
+        
+        let urlString: String = "https://www.androidthai.in.th/snru/addDataMaster.php?isAdd=true&Name=\(name)&User=\(user)&Password=\(password)"
         
         
-    } //myAlert
+        
+    }
     
     
     
